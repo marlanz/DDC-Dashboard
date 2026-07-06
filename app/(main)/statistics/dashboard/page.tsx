7821b6b9e57113ba1f9f313d1d8a6c9ffa553fe3
marlanz <phucanhdodang1211@gmail.com>
2026-07-06 16:41:39 +0700

    implement caching for /statistics/dashboard

--- app/(main)/statistics/dashboard/page.tsx
+++ app/(main)/statistics/dashboard/page.tsx
@@ -1,15 +1,16 @@
-import { connection } from "next/server";
-
 import { getAllEquipments } from "@/lib/data/equipments";
 import DashboardContent from "@/components/DashboardContent";
 import { Metadata } from "next";
+import { cacheLife } from "next/cache";
 
 export const metadata: Metadata = {
   title: "Bảng thống kê thiết bị |  Hệ thống Quản lí thiết bị Cơ khí",
 };
 
 export default async function DashboardPage() {
-  await connection();
+  "use cache";
+  cacheLife({ revalidate: 60 * 3 });
+  // await connection();
 
   const equipments = await getAllEquipments();
 
